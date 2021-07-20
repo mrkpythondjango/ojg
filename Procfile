fd1d7c2388dc971dcbc8e7af1d8583bfeac12b15
Rama Krishna Majety <mrkpythondjango@gmail.com>
2021-07-20 06:17:18 +0530

    20 July 2021

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn ojg.wsgi --log-file -
--- pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from .views import homePageView
-
-
-urlpatterns = [
-    path('', homePageView, name='home'),
-]
